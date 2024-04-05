2a05aef160a2f2ae4098c3098cd41edecc88cfd6
Deveus <raffree.dev@gmail.com>
2024-04-05 04:33:21 +0300

    minor changes

--- src/location/model.rs
+++ src/location/model.rs
@@ -1,5 +1,4 @@
 
-use clap::builder::Str;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug,Clone)]
@@ -10,17 +9,10 @@ pub struct Location {
 }
 
 impl Location {
-
     pub async fn resolve_location(city_name: &str) -> Result<Location, reqwest::Error> {
         let client = reqwest::Client::new();
 
         let url = format!("https://nominatim.openstreetmap.org/search?q={}&format=json", city_name);
-        log::debug!(
-            "Querying {} location info from {}",
-            city_name,
-            url
-        );
-
         let response = client
         .get(&url)
         .header(reqwest::header::USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36")
